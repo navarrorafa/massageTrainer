7b850497c95cd77d546723d10941c32ca26248d4
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2024-04-11 19:09:03 +0200

    fotos puesta

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import homeImage from '../assets/homePage.jpeg';
+
 import homeBg from '../assets/12.jpeg'
-import {IconsContacto} from '../components/IconsContacto'
+import homeMs1 from '../assets/05.jpeg'
+import homeMs2 from '../assets/10.jpeg'
+import homeT1 from '../assets/18.jpeg'
+import homeT2 from '../assets/19.jpeg'
+
+import { IconsContacto } from '../components/IconsContacto'
 
 
 
@@ -12,57 +17,82 @@ export const HomePage = () => {
 
             <section style={{ backgroundImage: `url(${homeBg})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                 <h1 className="text-center" style={{ fontSize: '5.5rem', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Massage Trainer Ibiza</h1>
-                <h3 className="text-center" style={{ fontSize: '3.5rem', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Sergio Oyrigoyen</h3>
-                <h2 className="text-center" style={{ fontSize: '2rem', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Quiromasaje / trainer</h2>
+                <h3 className="text-center" style={{ fontSize: '3.5rem', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Sergio Yrigyen</h3>
+                <h2 className="text-center" style={{ fontSize: '2rem', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Quiromasajista / Trainer</h2>
             </section>
 
             <section>
 
                 <p className='text-center m-5 fs-2'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores libero molestiae, voluptate id ea atque, quia officia eligendi quidem sint adipisci architecto sapiente optio minima, aliquid explicabo nostrum ex ut!
+                    Bienvenido a Massage Trainer Ibiza, soy Sergio Yrigoyen Pérez. Licenciado en actividad física y deporte, desarrolle mi carrera en el mundo del fitness y el entrenamiento físico. Desde hace 30 años me dedico profesionalmente al bienestar y al cuidado del cuerpo.
                 </p>
 
                 <p className='text-center m-5 fs-2'>
-                    Quaerat praesentium adipisci excepturi nobis minus eveniet ratione, modi atque magni alias qui consectetur ipsam delectus earum, vel numquam sapiente. Quas nulla eligendi unde eos quis aliquid sint! Harum, cupiditate.
+                    En el año 2008 comencé a desarrollarme como masajista hasta crear Massage Trainer Ibiza, una plataforma para el bienestar integral, combinando el masaje y la actividad física personalizada.
                 </p>
 
                 <p className='text-center m-5 fs-2'>
-                    Magnam, ut doloremque eaque rem non minima id corporis iusto itaque soluta, totam adipisci delectus praesentium deserunt dolore! Fugiat ipsum minima ullam in pariatur explicabo deleniti? Sapiente ex possimus atque?
-                    Illo at inventore ipsa aperiam nobis, autem, animi recusandae blanditiis magni, obcaecati assumenda delectus? Libero, ab enim obcaecati, corrupti consequuntur aut dolorem deleniti officiis exercitationem doloribus perferendis natus fugit iure.
+                    Massage Trainer Ibiza te ofrece una experiencia de masaje excepcional utilizando diferentes técnicas, que aprendí en mi vasta formación:
                 </p>
 
-            </section>
+                <ul className='text-start m-auto p fs-2'>
+                <li>Masajes Deep tissue</li>
+                <li>Masaje relajante</li>
+                <li>Masajes lomi lomi</li>
+                <li>Masaje tailandés</li>
+                <li>Reflexología podal</li>
+                <li>Masajes para embarazadas</li>
+            </ul>
+
+                <p className='text-center m-5 fs-2'>
+                    Trabajamos de manera consciente, brindando acompañamientos integrales para tu bienestar. Es por eso que también ofrecemos:
+                </p>
+
+                <ul className='text- m-auto  fs-2'>
+                <li>Entrenamiento físico personalizado</li>
+                <li>Entrenamiento físico acuático (lesiones / adultos mayores)</li>
+                <li>Clases recreativas acuáticas (adultos mayores y niños)</li>
+            </ul>
+
+                <p className='text-center m-5 fs-2'>
+                    Te invitamos a descubrir nuestro servicio personalizado y de alta calidad.
+                </p>
+
+                <p className='text-center m-5 fs-2'>
+                    ¡Bienvenidos a Massage Trainer Ibiza, el refugio de bienestar que transformará su vida!
+                </p>
+
+
+
+            </section >
 
             <section className='bg-secondary bg-opacity-75'>
-
-                <div className="d-flex justify-content-center flex-wrap gap-3 py-5">
-                    <div>
-                        <img src="https://img.freepik.com/fotos-gratis/close-up-feminino-nas-costas-fazendo-uma-massagem-de-repouso-horizontal_186202-4821.jpg" alt="" />
-                    </div>
-
-                    <div>
-                        <img src="https://media.istockphoto.com/id/482266425/pt/foto/massagem-na-horizontal.jpg?s=612x612&w=0&k=20&c=BIqTnE4gbA5Q2C8g9R-EbPpM2zWIunHY8HyGRTXhDiY=" alt="" />
-                    </div>
-
-                    <div>
-                        <img src="https://img.freepik.com/fotos-gratis/homem-a-dar-massagem-ao-paciente_23-2150649777.jpg?size=626&ext=jpg&ga=GA1.1.1687694167.1711756800&semt=ais" alt="" />
-                    </div>
-
-                    <div>
-                        <img src="https://thumbs.dreamstime.com/b/homem-sendo-massageado-no-salão-spa-horizontal-partir-de-cima-homens-irreconhecíveis-receber-massagem-relaxante-moderno-170508617.jpg" alt="" />
-                    </div>
-                </div>
-
+    <div className="container py-5">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-6 mb-3">
+                <img src={homeMs1} alt="" className="img-fluid w-100" />
+            </div>
+            <div className="col-12 col-md-6 mb-3">
+                <img src={homeMs2} alt="" className="img-fluid w-100" />
+            </div>
+            <div className="col-12 col-md-6 mb-3">
+                <img src={homeT1} alt="" className="img-fluid w-100" />
+            </div>
+            <div className="col-12 col-md-6 mb-3">
+                <img src={homeT2} alt="" className="img-fluid w-100" />
+            </div>
+        </div>
+    </div>
+</section>
 
 
-            </section>
 
             <section>
-         
-            <p className='fs-1 text-center p5'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, ab, delectus corrupti omnis architecto nesciunt voluptatum incidunt officiis odit molestiae at natus tenetur? Enim sed eveniet dolor at animi tempore.</p>
-  
+
+                <p className='fs-1 text-center p5'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, ab, delectus corrupti omnis architecto nesciunt voluptatum incidunt officiis odit molestiae at natus tenetur? Enim sed eveniet dolor at animi tempore.</p>
+
             </section>
 
-        </div>
+        </div >
     )
 }
--- src/pages/Masaje.jsx
+++ src/pages/Masaje.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import imgRelajante from  '../assets/03.jpeg'
+import imgDes from  '../assets/02.jpeg'
+import imgLomi from  '../assets/13.jpeg'
+import imgThai from  '../assets/17.jpeg'
+import imgEmb from  '../assets/15.jpeg'
+import imgPodal from  '../assets/16.jpeg'
+import imgVent from  '../assets/14.jpeg'
 
 export const Masaje = () => {
     return (
@@ -8,7 +15,7 @@ export const Masaje = () => {
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgRelajante} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Masaje Relajante</h5>
                                     <p className="card-text">Consiste en la realización de maniobras superficiales en las que la intensidad de la presión es suave y el ritmo lento y reiterativo</p>
@@ -17,9 +24,9 @@ export const Masaje = () => {
                         </div>
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgDes} className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Masaje Descontracturante</h5>
+                                    <h5 className="card-title">Masaje Deep Tissue</h5>
                                     <p className="card-text">Es una técnica de terapia manual, que consiste en emplear maniobras profundas e intensas, que van dirigidas a zonas de dolor y que buscan eliminar las contracturas y puntos gatillo, ayudando a recuperar la función normal del músculo.</p>
                                 </div>
                             </div>
@@ -33,7 +40,7 @@ export const Masaje = () => {
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgLomi} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Masaje Lomi Lomi</h5>
                                     <p className="card-text">Durante un masaje Lomi-Lomi o masaje hawaiano , se amasa todo el cuerpo y uno se siente casi majestuoso: los movimientos fluidos de la cabeza a los pies y el uso del cuerpo por parte del masajista garantizan una agradable relajación.</p>
@@ -43,7 +50,7 @@ export const Masaje = () => {
                         </div>
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgThai} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Masaje Thailandes</h5>
                                     <p className="card-text">El masaje thai reúne las técnicas de 3 disciplinas de tratamiento tradicional: Amasamiento de los músculos. Manipulación del esqueleto o quiropráctica (osteopatía). Digitopuntura, presionando sobre ciertos puntos de acupuntura o líneas energéticas.</p>
@@ -60,7 +67,7 @@ export const Masaje = () => {
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgEmb} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Masaje Embarazadas</h5>
                                     <p className="card-text">La embarazada puede someterse a masajes terapéuticos y a masajes relajantes, siempre a partir del 2º trimestre de gestación. Los masajes terapéuticos pueden aliviar dolores: no olvidemos que el 71% de las embarazadas sufre dolor lumbar, el 46% dolor ciático y el 65% dolor pélvico.</p>
@@ -69,7 +76,7 @@ export const Masaje = () => {
                         </div>
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgPodal} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Reflexologia Podal</h5>
                                     <p className="card-text">
@@ -85,18 +92,10 @@ export const Masaje = () => {
             <section>
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-2 g-4">
+                        
                         <div className="col">
                             <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Masaje Deep Tissue</h5>
-                                    <p className="card-text"> Es una técnica de terapia manual, que consiste en emplear maniobras profundas e intensas, que van dirigidas a zonas de dolor y que buscan eliminar las contracturas y puntos gatillo, ayudando a recuperar la función normal del músculo.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card h-100">
-                                <img src="..." className="card-img-top" alt="..." />
+                                <img src={imgVent} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Masaje con Ventosa</h5>
                                     <p className="card-text">
